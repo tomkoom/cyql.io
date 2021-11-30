1421e4ea049f3539d223766741cbb1090ca55396
tomkoom <tomash.sugint@gmail.com>
2021-12-01 02:19:14 +0300

    refactor: refactor AppList

--- src/icapps_assets/src/AppList/AppList.jsx
+++ src/icapps_assets/src/AppList/AppList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./AppList.css";
+import css from "./AppList.module.css";
 import { Link } from "react-router-dom";
 
 // FRAMER MOTION
@@ -10,138 +10,136 @@ import { cardVariants } from "../MotionVariants";
 import TagButton from "./TagButton/TagButton";
 
 const AppList = ({
-	category,
-	setCategory,
-	filteredApps,
-	data,
-	loading,
-	error,
+  category,
+  setCategory,
+  filteredApps,
+  data,
+  loading,
+  error,
 }) => {
-	const categories = [
-		{ name: "All", icon: "" },
-		{ name: "Social Networks", icon: "🎯" },
-		{ name: "Games", icon: "⚔️" },
-		{ name: "dApps", icon: "🔗" },
-		{ name: "DeFi", icon: "‍🌾" },
-		{ name: "DAOs", icon: "🏠" },
-		{ name: "Infrastructure", icon: "🚀" },
-		{ name: "Wallets", icon: "👛" },
-		{ name: "Tools", icon: "🛠️" },
-		{ name: "Explorers", icon: "🌎" },
-		{ name: "NFTs", icon: "🗿" },
-		{ name: "Dfinity Apps", icon: "♾️" },
-		{ name: "Communities", icon: "" },
-	];
+  const categories = [
+    { name: "All", icon: "" },
+    { name: "Social Networks", icon: "🎯" },
+    { name: "Games", icon: "⚔️" },
+    { name: "dApps", icon: "🔗" },
+    { name: "DeFi", icon: "‍🌾" },
+    { name: "DAOs", icon: "🏠" },
+    { name: "Infrastructure", icon: "🚀" },
+    { name: "Wallets", icon: "👛" },
+    { name: "Tools", icon: "🛠️" },
+    { name: "Explorers", icon: "🌎" },
+    { name: "NFTs", icon: "🗿" },
+    { name: "Dfinity Apps", icon: "♾️" },
+    { name: "Communities", icon: "" },
+  ];
 
-	return (
-		<div className="container1440">
-			{/* CATEGORY BUTTONS */}
+  return (
+    <div className="container1440">
+      {/* CATEGORY BUTTONS */}
 
-			<div className="tags">
-				{categories.map((cat, i) => (
-					<TagButton
-						handleSetCategory={setCategory}
-						category={cat.name}
-						categoryActive={category === cat.name ? true : false}
-						icon={cat.icon}
-						appsNum={
-							loading
-								? null
-								: cat.name === "All"
-								? data[0].data.length
-								: data[0].data.filter(
-										(item) => item.category === cat.name
-								  ).length
-						}
-						key={i}
-					/>
-				))}
-			</div>
+      <div className={css.tags}>
+        {categories.map((cat, i) => (
+          <TagButton
+            handleSetCategory={setCategory}
+            category={cat.name}
+            categoryActive={category === cat.name ? true : false}
+            icon={cat.icon}
+            appsNum={
+              loading
+                ? null
+                : cat.name === "All"
+                ? data[0].data.length
+                : data[0].data.filter((item) => item.category === cat.name)
+                    .length
+            }
+            key={i}
+          />
+        ))}
+      </div>
 
-			{/* APP LIST */}
-			{loading ? (
-				<p className="center">Loading... ⌛</p>
-			) : error ? (
-				<p className="center">Error!</p>
-			) : (
-				<div className="app-list">
-					{filteredApps.map((d) => (
-						<motion.div
-							key={d.id}
-							className="app-list__item"
-							variants={cardVariants}
-							whileHover="whileHover"
-						>
-							<Link className="link-block" to={`/a/${d.id}`}>
-								<div
-									className="app-cover"
-									style={
-										d.cover
-											? {
-													backgroundImage: `url(${d.cover})`,
-											  }
-											: {
-													display: "none",
-											  }
-									}
-								></div>
-								<div className="app-list__app-info">
-									<img
-										className="app-list__app-info__logo"
-										src={d.logo}
-										alt={d.name}
-										style={
-											d.logo ? null : { display: "none" }
-										}
-									/>
-									<div className="app-list__app-info__description">
-										<h2 className="app-list__app-info__description__title">
-											{d.name}
-											&nbsp;
-											{d.category === "Social Networks"
-												? "🎯"
-												: d.category === "Games"
-												? "⚔️"
-												: d.category === "dApps"
-												? "🔗"
-												: d.category === "DeFi"
-												? "‍🌾"
-												: d.category === "DAOs"
-												? "🏠"
-												: d.category ===
-												  "Infrastructure"
-												? "🚀"
-												: d.category === "Wallets"
-												? "👛"
-												: d.category === "Tools"
-												? "🛠️"
-												: d.category === "Explorers"
-												? "🌎"
-												: d.category === "NFTs"
-												? "🗿"
-												: d.category === "DeFi"
-												? "‍🌾"
-												: null}
-										</h2>
+      {/* APP LIST */}
+      {loading ? (
+        <p className="center">Loading... ⌛</p>
+      ) : error ? (
+        <p className="center">Error!</p>
+      ) : (
+        <div className={css.li}>
+          {filteredApps.map((d) => (
+            <motion.div
+              key={d.id}
+              className={css.li__item}
+              variants={cardVariants}
+              whileHover="whileHover"
+            >
+              <Link className={css.li__item__linkBlock} to={`/a/${d.id}`}>
+                <div
+                  className={css.li__item__linkBlock__coverImg}
+                  style={
+                    d.cover
+                      ? {
+                          backgroundImage: `url(${d.cover})`,
+                        }
+                      : {
+                          display: "none",
+                        }
+                  }
+                ></div>
+                <div className={css.li__item__linkBlock__appInfo}>
+                  <img
+                    className={css.li__item__linkBlock__appInfo__logo}
+                    src={d.logo}
+                    alt={d.name}
+                    style={d.logo ? null : { display: "none" }}
+                  />
+                  <div
+                    className={css.li__item__linkBlock__appInfo__description}
+                  >
+                    <h2
+                      className={
+                        css.li__item__linkBlock__appInfo__description__title
+                      }
+                    >
+                      {d.name}
+                      &nbsp;
+                      {d.category === "Social Networks"
+                        ? "🎯"
+                        : d.category === "Games"
+                        ? "⚔️"
+                        : d.category === "dApps"
+                        ? "🔗"
+                        : d.category === "DeFi"
+                        ? "‍🌾"
+                        : d.category === "DAOs"
+                        ? "🏠"
+                        : d.category === "Infrastructure"
+                        ? "🚀"
+                        : d.category === "Wallets"
+                        ? "👛"
+                        : d.category === "Tools"
+                        ? "🛠️"
+                        : d.category === "Explorers"
+                        ? "🌎"
+                        : d.category === "NFTs"
+                        ? "🗿"
+                        : d.category === "DeFi"
+                        ? "‍🌾"
+                        : null}
+                    </h2>
 
-										<p className="body-text">
-											{d.description &&
-											d.description.length > 90
-												? `${d.description.substring(
-														0,
-														90
-												  )}...`
-												: d.description}
-										</p>
-									</div>
-								</div>
-							</Link>
-						</motion.div>
-					))}
-				</div>
-			)}
-		</div>
-	);
+                    <p className="bodyText">
+                      {d.description && d.description.length > 90
+                        ? `${d.description.substring(0, 90)}...`
+                        : d.description}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </motion.div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AppList;
--- src/icapps_assets/src/AppList/TagButton/TagButton.jsx
+++ src/icapps_assets/src/AppList/TagButton/TagButton.jsx
@@ -1,29 +1,25 @@
 import React from "react";
-import "./TagButton.css";
+import css from "./TagButton.module.css";
 
 const TagButton = ({
-	category,
-	handleSetCategory,
-	categoryActive,
-	icon,
-	appsNum,
+  category,
+  handleSetCategory,
+  categoryActive,
+  icon,
+  appsNum,
 }) => {
-	return (
-		<button
-			className={`tags__item ${
-				categoryActive ? "tags__item__active" : null
-			}`}
-			onClick={() => handleSetCategory(category)}
-		>
-			{icon ? (
-				`${icon} `
-			) : category === "Dfinity Apps" ? (
-				<div className="icp-price-badge__logo"></div>
-			) : null}
-			{category}
-			<p className="apps-num">{appsNum}</p>
-		</button>
-	);
+  return (
+    <button
+      className={`${css.tags__item} ${
+        categoryActive ? css.tags__item__active : null
+      }`}
+      onClick={() => handleSetCategory(category)}
+    >
+      {icon ? `${icon} ` : null}
+      {category}
+      <span className={css.appsNum}>{appsNum}</span>
+    </button>
+  );
 };
 
 export default TagButton;
