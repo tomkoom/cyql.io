b90adada0e907acc53c516c9aef84f59acdd1115
tomkoom <tomash.sugint@gmail.com>
2021-12-05 03:49:22 +0300

    fix: update input states

--- src/icapps_assets/src/Homepage/Search/Search.jsx
+++ src/icapps_assets/src/Homepage/Search/Search.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Search.module.css";
 
+// FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 const Search = ({ setSearch, data, loading }) => {
+  const [isActive, setIsActive] = useState(false);
+  const [mouseOver, setMouseOver] = useState(false);
+
   return (
     <div className={css.search}>
       <input
@@ -9,6 +16,26 @@ const Search = ({ setSearch, data, loading }) => {
         type="text"
         placeholder={loading ? "" : `Search ${data[0].data.length} projects`}
         onChange={(e) => setSearch(e.target.value)}
+        onFocus={() => {
+          setIsActive(true); // true == black
+        }}
+        onBlur={() => {
+          setIsActive(false); // false == white
+        }}
+        onMouseEnter={() => setMouseOver(true)}
+        onMouseLeave={() => setMouseOver(false)}
+      />
+
+      <FontAwesomeIcon
+        className={css.searchIcon}
+        style={
+          isActive
+            ? { color: "#11100f" }
+            : mouseOver
+            ? { color: "white" }
+            : { color: "rgba(255, 255, 255, 0.33)" }
+        }
+        icon={faSearch}
       />
     </div>
   );
