d7b8ce07f94b68fd4fcccc950bc297f73e045c17
tomkoom <tomash.sugint@gmail.com>
2022-04-28 23:34:47 +0300

    feat: update admin table styles

--- src/icapps_assets/src/Pages/Admin/Admin.jsx
+++ src/icapps_assets/src/Pages/Admin/Admin.jsx
@@ -13,6 +13,21 @@ const Admin = () => {
   const [openModal, setOpenModal] = useState(false);
   const projects = useSelector(selectProjects);
 
+  const formatStr8 = (str) => {
+    return str.length > 8 ? `${str.substring(0, 8)}…` : str;
+  };
+
+  const formatStr16 = (str) => {
+    return str.length > 16 ? `${str.substring(0, 16)}…` : str;
+  };
+
+  const formatWebsite = (url) => {
+    let formattedURL = url.replace(/(^\w+:|^)\/\//, "");
+    formattedURL = formattedURL.replace(/\/$/, ""); // remove trailing slash
+
+    return formattedURL.length > 12 ? `${formattedURL.substring(0, 16)}…` : formattedURL;
+  };
+
   const handleString = (string) => {
     let str = string;
     str = str.includes("https://")
@@ -27,6 +42,26 @@ const Admin = () => {
     return str;
   };
 
+  const getTwitterUsername = (url) => {
+    const username = url.split(".com/")[1];
+    return username;
+  };
+
+  const formatDiscord = (url) => {
+    if (url.includes("discord.gg/")) {
+      return url.split("discord.gg/")[1];
+    }
+    if (url.includes("discord.com/")) {
+      return url.split("discord.com/")[1];
+    }
+    return url;
+  };
+
+  const formatDate = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleString("en-GB");
+  };
+
   return (
     <div className={css.admin}>
       <div className={css.title}>
@@ -36,41 +71,43 @@ const Admin = () => {
         </button>
       </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>idx</th>
-            <th>id</th>
-            <th>name</th>
-            <th>category</th>
-            <th>website</th>
-            <th>canister</th>
-            <th>logo</th>
-            <th>cover</th>
-            <th>twitter</th>
-            <th>discord</th>
-            <th>added</th>
-          </tr>
-        </thead>
-        <tbody>
-          {projects.map((project) => (
-            <tr key={project.idx}>
-              <td>{project.idx && handleString(project.idx)}</td>
-              <td>{project.id && handleString(project.id)}</td>
-              <td>{project.name && handleString(project.name)}</td>
-              <td>{project.category}</td>
-              <td>{project.website && handleString(project.website)}</td>
-              <td>{project.canister && handleString(project.canister)}</td>
-              <td>{project.logo && handleString(project.logo)}</td>
-              <td>{project.cover && handleString(project.cover)}</td>
-              <td>{project.twitter && handleString(project.twitter)}</td>
-              <td>{project.discord && handleString(project.discord)}</td>
-              <td>{project.added && Date(project.added * 1000)}</td>
-              {/* <td>{project.added ? project.added : ""}</td> */}
+      <div style={{ overflow: "auto" }}>
+        <table>
+          <thead>
+            <tr>
+              <th>Idx</th>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Website</th>
+              <th>Canister</th>
+              <th>Logo</th>
+              <th>Cover</th>
+              <th>Twitter</th>
+              <th>Discord</th>
+              <th>Added</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {projects.map((project) => (
+              <tr key={project.idx}>
+                <td>{project.idx && formatStr8(project.idx)}</td>
+                <td>{project.id && formatStr16(project.id)}</td>
+                <td>{project.name && formatStr16(project.name)}</td>
+                <td>{project.category}</td>
+                <td>{project.website && formatWebsite(project.website)}</td>
+                <td>{project.canister && handleString(project.canister)}</td>
+                <td>{project.logo && handleString(project.logo)}</td>
+                <td>{project.cover && handleString(project.cover)}</td>
+                <td>{project.twitter && getTwitterUsername(project.twitter)}</td>
+                <td>{project.discord && formatDiscord(project.discord)}</td>
+                <td>{project.added ? formatDate(project.added) : ""}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
       {openModal && <AddProjectModal openModal={openModal} setOpenModal={setOpenModal} />}
     </div>
   );
