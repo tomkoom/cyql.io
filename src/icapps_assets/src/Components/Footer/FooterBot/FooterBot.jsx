2717d7c2f9382d8bc114fabcb7ef7940b4039ab5
tomkoom <tomash.sugint@gmail.com>
2022-06-10 13:49:47 +0300

    refactor: refactor footer top section

--- src/icapps_assets/src/Components/Footer/FooterBot/FooterBot.jsx
+++ src/icapps_assets/src/Components/Footer/FooterBot/FooterBot.jsx
@@ -7,7 +7,7 @@ const FooterBot = () => {
     <div className={css.footerBot}>
       <p>Powered by</p>
       <a href="https://dfinity.org/" target="_blank" rel="noreferrer noopener">
-        <img className={css.icLogo} src={ICLogo} alt="Internet-Computer-logo" />
+        <img className={css.icLogo} src={ICLogo} alt="Internet Computer logo" />
         Internet Computer
       </a>
     </div>
--- src/icapps_assets/src/Components/Footer/FooterTop/FooterTop.jsx
+++ src/icapps_assets/src/Components/Footer/FooterTop/FooterTop.jsx
@@ -12,27 +12,27 @@ const FooterTop = () => {
   return (
     <ul className={css.footerTop}>
       {/* submit */}
-      <li className={css.footerTop__i}>
-        <button className={`${css.footerTop__i__linkBlock} navlink`} onClick={toSubmit}>
-          <div>{iPlusSquare}</div>
+      <li className={css.footerTopI}>
+        <div className={css.linkBlock} onClick={toSubmit}>
+          <span>{iPlusSquare}</span>
           <div>
-            <h5 className={css.footerTop__i__linkBlock__title}>Submit your project</h5>
+            <h5 className={css.title}>Submit your project</h5>
             <p className="bodyText">Submit your project and get traction from the IC community.</p>
           </div>
-        </button>
+        </div>
       </li>
 
       {/* reach out */}
-      <li className={css.footerTop__i}>
+      <li className={css.footerTopI}>
         <a
-          className={css.footerTop__i__linkBlock}
+          className={css.linkBlock}
           href="https://twitter.com/messages/compose?recipient_id=1386304698358116354"
           rel="noreferrer noopener"
           target="_blank"
         >
-          <div>{iComments}</div>
+          <span>{iComments}</span>
           <div>
-            <h5 className={css.footerTop__i__linkBlock__title}>Reach out</h5>
+            <h5 className={css.title}>Reach out</h5>
             <p className="bodyText">
               Didn't find what you were looking for or want to collaborate? Reach out for us, we are
               happy to support and cooperate.
@@ -42,14 +42,13 @@ const FooterTop = () => {
       </li>
 
       {/* donate */}
-      <li className={css.footerTop__i__donation}>
-        <div>{iHeart}</div>
+      <li className={css.footerTopI__donation}>
+        <span>{iHeart}</span>
         <div>
-          <h5 className={css.footerTop__i__linkBlock__title}>Donate</h5>
+          <h5 className={css.title}>Donate</h5>
           <p className="bodyText">
             icApps is developed and maintained by the IC enthusiasts. You can support the project by
             making a donation.
-            <br />
             <span>
               <a
                 href="https://dashboard.internetcomputer.org/account/edf5163b9cc9084ae504ef56c239b0bfb6afbbc6e6e7c88e9cb3069fb2e135c1"
