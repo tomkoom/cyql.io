e2d8a3e141701c4c954cb7113521d4f87b4c361d
tomkoom <tomash.sugint@gmail.com>
2023-12-25 21:40:08 +0700

    update add/edit project

--- src/frontend/pages/admin/Admin.tsx
+++ src/frontend/pages/admin/Admin.tsx
@@ -1,43 +1,44 @@
-import React, { FC } from "react";
-import styled from "styled-components";
-import { device } from "@/styles/breakpoints";
+import React, { FC } from "react"
+import styled from "styled-components"
+import { device } from "@/styles/breakpoints"
 
 // components
-import { Projects } from "./_index";
-import { Btn } from "@/components/btns/_index";
+import { Projects } from "./_index"
+import { Btn } from "@/components/btns/_index"
 
 // state
-import { useAppDispatch } from "@/hooks/useRedux";
-import { setProjectModalIsOpen } from "@/state/modals/projectModal";
+import { useAppDispatch } from "@/hooks/useRedux"
+import { setProjectModalMode, setProjectModalIsOpen } from "@/state/modals/projectModal"
 
 const Admin: FC = (): JSX.Element => {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
-  const openModal = (): void => {
-    dispatch(setProjectModalIsOpen(true));
-  };
+  const openAddProjectModal = (): void => {
+    dispatch(setProjectModalMode("add"))
+    dispatch(setProjectModalIsOpen(true))
+  }
 
   return (
     <AdminStyled>
       <Title>
         <h2 className="pageTitle">admin</h2>
-        <Btn btnType="primary" text="add project" onClick={openModal} />
+        <Btn btnType="primary" text="add project" onClick={openAddProjectModal} />
       </Title>
       <Projects />
     </AdminStyled>
-  );
-};
+  )
+}
 
 const AdminStyled = styled.div`
   @media ${device.laptop} {
     padding: 0 1rem;
   }
-`;
+`
 
 const Title = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-`;
+`
 
-export default Admin;
+export default Admin
