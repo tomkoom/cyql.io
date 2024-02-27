a669db2f3b73b776c588dff5f468b34983bef91e
tomkoom <tomash.sugint@gmail.com>
2024-02-27 22:39:15 +0700

    rm unused packages

--- src/frontend/utils/projectId.ts
+++
@@ -1,7 +0,0 @@
-// nanoid https://github.com/ai/nanoid
-import { customAlphabet } from "nanoid"
-
-const alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
-const numbers = "0123456789"
-const size = 12
-export const projectId: () => string = customAlphabet(alphabet + numbers, size)
