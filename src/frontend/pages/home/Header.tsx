453678d16adda6c6ce7cc30a348f4e1c429ae8dc
tomkoom <tomash.sugint@gmail.com>
2025-07-09 17:01:27 +0700

    update header

--- src/frontend/pages/home/Header.tsx
+++ src/frontend/pages/home/Header.tsx
@@ -45,18 +45,22 @@ export default function Header() {
 
   return (
     <header className="flex flex-col items-center justify-center space-y-2 text-center">
-      <h2 className="text-5xl leading-tight font-black">
+      <h1 className="text-5xl leading-tight font-black">
         <span className="text-accent-1">Internet Computer</span> Ecosystem Playground
-      </h2>
-      <ul className="flex flex-wrap justify-center gap-2 text-sm text-neutral-500">
+      </h1>
+      <p className="text-coolgray-500 max-w-3xl">
+        Explore cutting-edge Web3 projects built on the Internet Computer—gaming, AI agents, metaverse, social dApps, betting, DeFi, and beyond. Keep up with
+        the next big thing in decentralized tech.
+      </p>
+      <ul className="text-coolgray-500 flex flex-wrap justify-center gap-2 text-sm">
         {HEADER_ELEMENTS.map((el) => (
-          <li key={el.text} className="flex items-center gap-1 rounded-full bg-neutral-900/50 px-3 py-1.5">
+          <li key={el.text} className="bg-coolgray-950/80 flex items-center gap-1 rounded-full px-3 py-1.5">
             {el.icon && <Icon lucideName={el.icon as LucideIconsKeys} size={16} />}
             <span>{el.text}</span>
           </li>
         ))}
       </ul>
-      <p className="text-coolgray-500 text-2xl">
+      <p className="text-coolgray-500 text-xl">
         Explore{" "}
         <span className="text-accent-1 hover:text-accent-2 cursor-pointer font-bold underline transition-all duration-300" onClick={toProjects}>
           {projectsNum > 0 ? projectsNum : "..."}
