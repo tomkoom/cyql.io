96fc5f926cc795aff32a2b7e5d9a74636b7d4969
tomkoom <tomash.sugint@gmail.com>
2025-07-20 23:44:50 +0700

    update homepage

--- src/frontend/components/ui/UpvotesNum.tsx
+++ src/frontend/components/ui/UpvotesNum.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from "react"
-import styled from "styled-components"
 import { iArrowUp } from "@/components/icons/Icons"
+import { FC } from "react"
+import styled from "styled-components"
 
 interface UpvotesNumProps {
   upvotesNum: number
--- src/frontend/pages/home/HighlightedProjects/Project.tsx
+++ src/frontend/pages/home/HighlightedProjects/Project.tsx
@@ -1,5 +1,5 @@
 import { iCircleNodes, iGithub } from "@/components/icons/Icons"
-import { UpvotesNum } from "@/components/ui"
+import { Icon } from "@/components/ui/Icon"
 import { useNav } from "@/hooks"
 import type { Project as ProjectType } from "@/state/types/Project"
 import { Logo } from "."
@@ -22,7 +22,12 @@ export default function Project({ project }: ProjectProps) {
         <Logo name={project.name} logo={project.logoDataUrl} />
         <div className="w-full">
           <div className="flex items-baseline justify-between gap-1">
-            <h4 className="group-hover:text-accent-3 line-clamp-1 font-bold text-wrap transition-colors">{name}</h4> <UpvotesNum upvotesNum={upvotesNum} />
+            <h4 className="group-hover:text-accent-3 line-clamp-1 font-bold text-wrap transition-colors">{name}</h4>
+            {upvotesNum > 0 && (
+              <p className="flex items-center gap-1 rounded-sm bg-emerald-500/10 px-1 py-0.5 text-xs font-bold text-emerald-500 tabular-nums">
+                <Icon lucideName="ArrowUp" size={14} strokeWidth={2} /> {upvotesNum}
+              </p>
+            )}
           </div>
           <p className="text-coolgray-500 line-clamp-2 text-sm font-light">{description}</p>
         </div>
@@ -32,22 +37,22 @@ export default function Project({ project }: ProjectProps) {
         <div className="flex flex-wrap items-center gap-2">
           <ul className="flex flex-wrap items-center gap-1">
             {category.map((category) => (
-              <li key={category} className="bg-coolgray-900 text-coolgray-300 rounded-sm px-1 py-0.5 !font-mono text-xs">
+              <li key={category} className="bg-coolgray-900/60 text-coolgray-300 flex h-[22px] items-center rounded-sm px-1 !font-mono text-xs">
                 {category}
               </li>
             ))}
           </ul>
 
           {(frontendCanisterId || github) && (
-            <ul className="flex flex-wrap items-center gap-2">
+            <ul className="flex flex-wrap items-center">
               {frontendCanisterId && (
-                <li className="text-coolgray-300 font-mono text-sm">
-                  <span className="text-green-500">{iCircleNodes}</span> onchain
+                <li className="text-coolgray-300 bg-coolgray-900/60 text-coolgray-300 flex h-[22px] items-center gap-1 rounded-sm px-1 !font-mono text-xs">
+                  <span className="text-sm text-green-700">{iCircleNodes}</span> Onchain
                 </li>
               )}
               {github && (
-                <li className="text-coolgray-300 text-sm">
-                  <span className="text-blue-500">{iGithub}</span> open
+                <li className="text-coolgray-300 bg-coolgray-900/60 text-coolgray-300 flex h-[22px] items-center gap-1 rounded-sm px-1 !font-mono text-xs">
+                  <span className="text-sm text-blue-700">{iGithub}</span> Open
                 </li>
               )}
             </ul>
